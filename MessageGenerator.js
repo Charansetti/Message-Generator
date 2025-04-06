31f559a30aff8ea5418ecfee97631c60a742a6f0
Setti Charan Sai <setticharansai17@gmail.com>
2025-04-06 19:43:02 +0530

    Generator Welcome Messages to user

---
+++ MessageGenerator.js
@@ -0,0 +1,10 @@
+let messages = [
+  "Hi Hello",
+  "Hello World",
+  "Good Morning",
+  "Good Evening",
+  "Hello there, How can i help you",
+  "Bye dude",
+];
+let message = messages[Math.floor(Math.random() * messages.length)];
+console.log(message);
